b372e43ab935c25f2664a178d753847ad7c7cdda
milu234 <milanhazra234.mh@gmail.com>
2020-04-24 13:07:51 +0530

    data Fetcehd From API

--- corona/src/App.js
+++ corona/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Cards , Chart , CountryPicker} from './components';
+import styles from './App.module.css';
+import { fetchdata } from './api';
 
 class App extends React.Component{
+
+    //Request to fetch data
+   async componentDidMount(){
+       const data  = await fetchdata();
+
+       console.log(data);
+
+    }
+
+
     render(){
         return(
-            <div>
-                <h1>APP</h1>
+            <div className = {styles.container} >
+                <Cards />
+                <Chart />
+                <CountryPicker />
             </div>
         )
     }
